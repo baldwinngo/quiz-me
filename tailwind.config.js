55ccd362b3ffd4500da6c7f8125a78976cb6a630
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-04-14 22:10:52 -0700

    add tailwind and some base styles

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["index.html", "app.js"],
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#f5faff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
